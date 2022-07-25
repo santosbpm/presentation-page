e6f9ef64ac85497b7f91a4267d3a121bf963d86d
santosbpm <santosbpm.0@gmail.com>
2022-07-25 04:16:18 -0300

    script com evento ao clicar na imagem e section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function abrir() {
+    var about = document.getElementById("about");
+    about.classList.toggle("appear");
+}
+
+function fechar() {
+    var about = document.getElementById("about");
+    about.classList.toggle("appear");
+}
